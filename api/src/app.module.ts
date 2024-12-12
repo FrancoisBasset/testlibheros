100cd7aaf42c2028cf6e59d9179c01687e5b2fe5
FrancoisBasset <mrfrancoisbasset@gmail.com>
2024-12-12 13:19:52 +0100

    feat(api): add beginning of auth module, signIn method

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import AuthModule from './auth/auth.module';
 import UserModule from './user/user.module';
 import User from './user/entities/user.entity';
 
 @Module({
 	imports: [
+		AuthModule,
 		UserModule,
 		SequelizeModule.forRoot({
 			dialect: 'sqlite',
---
+++ api/src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import AuthController from './auth.controller';
+import AuthService from './auth.service';
+import UserModule from '../user/user.module';
+import { JwtService, JwtModule } from '@nestjs/jwt';
+
+@Module({
+	imports: [
+		UserModule,
+		JwtModule.register({
+			global: true,
+			secret: 'secret',
+			secretOrPrivateKey: 'secret',
+			signOptions: { expiresIn: '60s' }
+		})
+	],
+	controllers: [AuthController],
+	providers: [AuthService, JwtService]
+})
+export default class AuthModule {}
